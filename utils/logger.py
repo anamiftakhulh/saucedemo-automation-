83d9e758c1a6d32944193c2d2d87f48ec835d471
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:17:25 +0700

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,18 @@
+import logging
+import os
 
+def get_logger():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    logger = logging.getLogger("SauceDemoLogger")
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler("logs/test_execution.log")
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+
+    return logger
