7c0698094fc20f01dda7a1b7826dce7f9fa6b975
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:19:58 +0700

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,17 @@
+from pages.login_page import LoginPage
 
+
+def test_login_valid(driver):
+    login = LoginPage(driver)
+    login.load()
+    login.login("standard_user", "secret_sauce")
+
+    assert "inventory" in driver.current_url
+
+
+def test_login_invalid(driver):
+    login = LoginPage(driver)
+    login.load()
+    login.login("invalid_user", "secret_sauce")
+
+    assert "Epic sadface" in login.get_error()
