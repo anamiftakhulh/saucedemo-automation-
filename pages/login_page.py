f73e4683fd2cf398ccae6315930d79383bea48e2
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:19:26 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,1 +1,23 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
+
+class LoginPage(BasePage):
+
+    URL = "https://www.saucedemo.com"
+
+    USERNAME = (By.ID, "user-name")
+    PASSWORD = (By.ID, "password")
+    LOGIN_BTN = (By.ID, "login-button")
+    ERROR_MSG = (By.XPATH, "//h3[@data-test='error']")
+
+    def load(self):
+        self.open(self.URL)
+
+    def login(self, username, password):
+        self.type(*self.USERNAME, username)
+        self.type(*self.PASSWORD, password)
+        self.click(*self.LOGIN_BTN)
+
+    def get_error(self):
+        return self.get_text(*self.ERROR_MSG)
