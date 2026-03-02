5ce317c6a0bb18e6aa924cbe6e147f1d3da05fc5
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:18:59 +0700

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,1 +1,22 @@
+from selenium.webdriver.common.by import By
 
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def click(self, by, locator):
+        self.driver.find_element(by, locator).click()
+
+    def type(self, by, locator, text):
+        self.driver.find_element(by, locator).clear()
+        self.driver.find_element(by, locator).send_keys(text)
+
+    def get_text(self, by, locator):
+        return self.driver.find_element(by, locator).text
+
+    def is_visible(self, by, locator):
+        return self.driver.find_element(by, locator).is_displayed()
