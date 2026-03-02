1a426a781e74a6d93ba99f555fb7d6f75d73fa68
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:33:16 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,20 +4,22 @@ from pages.base_page import BasePage
 
 class LoginPage(BasePage):
 
-    URL = "https://www.saucedemo.com"
+    URL = "https://www.saucedemo.com/"
 
     USERNAME = (By.ID, "user-name")
     PASSWORD = (By.ID, "password")
     LOGIN_BTN = (By.ID, "login-button")
-    ERROR_MSG = (By.XPATH, "//h3[@data-test='error']")
 
     def load(self):
-        self.open(self.URL)
+        self.driver.get(self.URL)
 
     def login(self, username, password):
         self.type(*self.USERNAME, username)
         self.type(*self.PASSWORD, password)
         self.click(*self.LOGIN_BTN)
 
-    def get_error(self):
-        return self.get_text(*self.ERROR_MSG)
+    def is_login_page_displayed(self):
+        return self.is_visible(*self.USERNAME)
+
+    def is_visible(self, by, locator):
+        return self.driver.find_element(by, locator).is_displayed()
