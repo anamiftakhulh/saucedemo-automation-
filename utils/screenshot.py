fa5af33c201539728ad2f4b1649cd9d3bf3792f2
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:17:57 +0700

    Update screenshot.py

--- utils/screenshot.py
+++ utils/screenshot.py
@@ -1,1 +1,10 @@
+import os
+from datetime import datetime
 
+def take_screenshot(driver, test_name):
+    if not os.path.exists("screenshots"):
+        os.makedirs("screenshots")
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = f"screenshots/{test_name}_{timestamp}.png"
+    driver.save_screenshot(file_name)
