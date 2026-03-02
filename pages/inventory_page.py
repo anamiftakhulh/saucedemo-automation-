4b37c0ad72889f9ec8d7c8a6ad27e88e6e98a408
Ana Miftakhul H. <39774371+anamiftakhulh@users.noreply.github.com>
2026-03-02 23:25:24 +0700

    Update inventory_page.py

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,23 +1,63 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from pages.base_page import BasePage
 
 
 class InventoryPage(BasePage):
 
     SORT_DROPDOWN = (By.CLASS_NAME, "product_sort_container")
-    ADD_TO_CART_BTN = (By.XPATH, "//button[text()='Add to cart']")
+    ADD_TO_CART_BTNS = (By.XPATH, "//button[text()='Add to cart']")
+    REMOVE_BTNS = (By.XPATH, "//button[text()='Remove']")
     CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
+    CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
+
     MENU_BTN = (By.ID, "react-burger-menu-btn")
     LOGOUT_LINK = (By.ID, "logout_sidebar_link")
 
-    def sort_by(self, value):
-        self.driver.find_element(*self.SORT_DROPDOWN).send_keys(value)
+    PRODUCT_NAMES = (By.CLASS_NAME, "inventory_item_name")
+    PRODUCT_PRICES = (By.CLASS_NAME, "inventory_item_price")
+
+    # ---------- SORTING ----------
+
+    def sort_by_visible_text(self, text):
+        dropdown = Select(self.driver.find_element(*self.SORT_DROPDOWN))
+        dropdown.select_by_visible_text(text)
+
+    def get_product_names(self):
+        elements = self.driver.find_elements(*self.PRODUCT_NAMES)
+        return [el.text for el in elements]
+
+    def get_product_prices(self):
+        elements = self.driver.find_elements(*self.PRODUCT_PRICES)
+        return [float(el.text.replace("$", "")) for el in elements]
+
+    # ---------- CART ACTIONS ----------
 
     def add_first_product(self):
-        self.driver.find_elements(*self.ADD_TO_CART_BTN)[0].click()
+        buttons = self.driver.find_elements(*self.ADD_TO_CART_BTNS)
+        if buttons:
+            buttons[0].click()
+
+    def add_all_products(self):
+        buttons = self.driver.find_elements(*self.ADD_TO_CART_BTNS)
+        for button in buttons:
+            button.click()
+
+    def remove_all_products(self):
+        buttons = self.driver.find_elements(*self.REMOVE_BTNS)
+        for button in buttons:
+            button.click()
 
     def get_cart_count(self):
-        return self.get_text(*self.CART_BADGE)
+        badges = self.driver.find_elements(*self.CART_BADGE)
+        if badges:
+            return badges[0].text
+        return "0"
+
+    def open_cart(self):
+        self.click(*self.CART_ICON)
+
+    # ---------- LOGOUT ----------
 
     def logout(self):
         self.click(*self.MENU_BTN)
